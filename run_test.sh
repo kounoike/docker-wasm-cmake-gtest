d877fadf8509c36eaa4cc9b5b84873ec67836ab5
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-12-02 20:58:56 +0000

    run_test.sh アップデート

--- run_test.sh
+++ run_test.sh
@@ -5,17 +5,9 @@ shift
 
 # Chrome
 # なぜかremote-debugging-portをつけないと動かない。rootのときは--no-sandboxも付ける
-# emrun --browser chrome --browser_args="--headless --no-sandbox --disable-gpu --remote-debugging-port=9222" ${HTML} -- "$@" 2>/dev/null
-emrun --browser chrome --browser_args="--headless --disable-gpu --remote-debugging-port=9222" ${HTML} -- "$@" 2>/dev/null
-RESULT=$?
-# プロセスが残ってるので掃除する
-for pid in $(pidof google-chrome-stable); do kill $pid; done
+# exec emrun --browser chrome --browser_args="--headless --no-sandbox --disable-gpu --remote-debugging-port=9222" --kill_exit ${HTML} -- "$@" 2>/dev/null
+exec emrun --browser chrome --browser_args="--headless --disable-gpu --remote-debugging-port=9222" --kill_exit ${HTML} -- "$@" 2>/dev/null
 
 # Firefox
-# Firefoxは多重起動できないので実行前にも掃除しておく
-# for pid in $(pidof firefox); do kill $pid; done
-# emrun --browser firefox --browser_args="--headless" ${HTML} -- "$@"
-# RESULT=$?
-# for pid in $(pidof firefox); do kill $pid; done
-
-exit $RESULT
+# Firefoxは多重起動できないので念のためstartでもkillしておく
+# exec emrun --browser firefox --browser_args="--headless" --kill_start --kill_exit ${HTML} -- "$@"
